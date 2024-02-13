ebc3867e7efc5f774c40eafaead6ca263d070945
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2024-02-13 21:08:32 +0100

    added reg and login function

--- js/index.mjs
+++ js/index.mjs
@@ -5,6 +5,8 @@ export const API_REGISTER = "/register";
 export const API_LOGIN = "/login";
 export const API_KEY_URL = "/create-api-key";
 
+// export const API_KEY = ""
+
 
 //local storage functions
 export function save(key, value) {
@@ -26,26 +28,25 @@ export function load(key) {
 // }
 
 // function getAPIKey
-// export async function getAPIKey() {
-// 	const response = await fetch(API_BASE + API_AUTH + API_KEY_URL, {
-// 		method: "POST",
-// 		headers: {
-// 			"Content-Type": "application/json",
-// 			Authorization: `Bearer ${load("token")}`
-// 		},
-// 		body: JSON.stringify({
-// 			name: "Test Key"
-// 		})
-// 	}); 
+export async function getAPIKey() {
+	const response = await fetch(API_BASE + API_AUTH + API_KEY_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${load("token")}`
+		},
+		body: JSON.stringify({
+			name: "Test Key"
+		})
+	}); 
 
+	if (response.ok) {
+		return await response.json();
+	}
 
-// 	if (response.ok) {
-// 		return await response.json();
-// 	}
-
-// 	console.error(await response.json());
-// 	throw new Error("Could not register for an API key");
-// }
+	console.error(await response.json());
+	throw new Error("Could not register for an API key");
+}
 
 
 //register user function
@@ -55,36 +56,40 @@ export async function registerUser(name, email, password) {
 				"Content-Type": "application/json"
 			},
 			method: "POST",
-			body: JSON.stringify({ name, email, password })
+			body: JSON.stringify({ name, email, password }),
 	});
 
+	console.log(response);
+	const data = response.json();
+	console.log(data);
+
 	if (response.ok) {
-		return await response.json();
+		return data;	
 	}
 	throw new Error("Could not register the account");
 }
 
-
-
 // // login user function
-// export async function loginUser(email, password) {
-// 	const response = await fetch(API_BASE + API_AUTH + API_LOGIN, {
-// 		headers: {
-// 			"Content-Type": "application/json"
-// 		},
-// 		method: "POST",
-// 		body: JSON.stringify({ email, password })
-// 	});
+export async function loginUser(email, password) {
+	const response = await fetch(API_BASE + API_AUTH + API_LOGIN, {
+		headers: {
+			"Content-Type": "application/json"
+		},
+		method: "POST",
+		body: JSON.stringify({ email, password })
+	});
 
-// 		if (response.ok) {
-// 		const { accessToken, ...profile } = await response.json().data;
-// 		save("token", accessToken);
-// 		save("profile", profile);
-// 		return profile;
-// 	}
+	console.log(response);
 
-// 	throw new Error("Could not login");
-// }
+		if (response.ok) {
+			const data = await response.json();
+			const { accessToken, ...profile } = await data.data;
+			save("token", accessToken);
+			save("profile", profile);
+			return profile;
+		}
+		throw new Error("Could not login");
+	}
 
 
 //create an API key
@@ -116,13 +121,8 @@ export async function onAuth(event) {
 	const password = event.target.password.value;
 
 	registerUser(name, email, password);
+	loginUser(email, password);
 }
-
-// 	if (event.submitter.dataset.auth === "login") {
-// 		await login(email, password);
-// 	} else {
-// 		await register(name, email, password);
-// 	}
 
 // 	const posts = await getPosts();
 // 	console.log(posts);
