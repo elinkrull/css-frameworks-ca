b77d34e87f536f8e148c9456051a789315775ffc
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2024-03-08 09:19:17 +0100

    added display posts function, not working

--- js/api/posts/read.mjs
+++ js/api/posts/read.mjs
@@ -1,6 +1,38 @@
 import { API_BASE, API_POSTS } from "../constants.mjs";
 import { authFetch } from "../authfetch.mjs";
 
+
+
+const generateSinglePostHtml = (post) => {
+	console.log(post);
+
+	const postContainer = document.createElement("div");
+
+	const postTitle = document.createElement("h4");
+	postTitle.textContent = post.title;
+
+	const postBody = document.createElement("p");
+	postBody.textContent = post.body;
+
+	postContainer.appendChild(postTitle, postBody);
+
+	return postContainer;
+}
+
+
+//display posts function
+export function displayPosts(posts) {
+
+	const postsDisplayContainer = document.querySelector("#posts-display-container");
+	postsDisplayContainer.textContent = "";
+	
+
+	posts.forEach((post, index) => {
+		const postHtml = generateSinglePostHtml(post);
+		postsDisplayContainer.appendChild(postHtml);
+	});
+}
+
 //get posts function
 export async function getPosts() {
 	const response = await authFetch(API_BASE + API_POSTS);
@@ -15,6 +47,6 @@ export async function getPost(id) {
 	const getPostURL = `${API_BASE}${API_POSTS}/${id}`;
 
 	const response = await authFetch(getPostURL)
-
 	return await response.json();
 }
+
--- js/index.mjs
+++ js/index.mjs
@@ -1,6 +1,14 @@
 import { homePage } from "./routes/home.mjs";
+import * as post from "./api/posts/index.mjs";
 
 homePage()
 
 
+post.getPosts().then(console.log)
 
+
+// post.removePost(869).then(console.log)
+
+post.getPost(839).then(console.log)
+
+// post.displayPosts().then(console.log)
